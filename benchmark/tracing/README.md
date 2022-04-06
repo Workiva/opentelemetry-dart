cf73d2bc733554a0700906cfad164797ac01a682
evanweible-wf <evan.weible@workiva.com>
2022-04-06 16:21:34 -0600

    Add simple benchmark for tracing

---
+++ benchmark/tracing/README.md
@@ -0,0 +1,20 @@
+# Tracing Benchmark
+
+## Dart VM
+```bash
+dart run benchmark/tracing/main.dart
+```
+
+## Browser
+
+DDC:
+```bash
+webdev serve benchmark
+```
+
+dart2js:
+```bash
+webdev serve benchmark -r
+```
+
+Then open http://localhost:8080/tracing/
---
+++ benchmark/tracing/main.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:benchmarking/benchmarking.dart';
+import 'package:opentelemetry/sdk.dart' as otel_sdk;
+
+void main() async {
+  final tracer = otel_sdk.globalTracerProvider.getTracer('benchmark');
+
+  final result = await asyncBenchmark('Tracing (Zone.fork per span)', () async {
+    // Sync trace
+    tracer.trace('1[sync]', () => 1);
+
+    // Async trace
+    await tracer.trace('2[async]', () async => 2);
+
+    // Two interleaved async traces
+    final trace3Completer = Completer();
+    tracer.trace('3[async]', () => trace3Completer.future);
+    final trace4Completer = Completer();
+    tracer.trace('4[async]', () => trace4Completer.future);
+    trace4Completer.complete();
+    trace3Completer.complete();
+
+    // A tree of async traces with depth=100
+    Future<void> recursiveTraceTo(int maxDepth, [int currentDepth]) async {
+      currentDepth ??= 0;
+      if (currentDepth >= maxDepth) return;
+      tracer.trace('depth=$currentDepth',
+          () => recursiveTraceTo(maxDepth, currentDepth + 1));
+    }
+
+    await recursiveTraceTo(100);
+  });
+  result.report(units: 104);
+}
