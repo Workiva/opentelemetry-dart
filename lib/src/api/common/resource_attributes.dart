fb2388bb9a169e33cf8f60e2420b52d2c9b3867b
Michael Yeager <michael.yeager@webfilings.com>
2022-06-03 14:37:16 -0500

    Add Resource Attribute for Deployment Environment

--- lib/src/api/common/resource_attributes.dart
+++ lib/src/api/common/resource_attributes.dart
@@ -3,6 +3,9 @@
 /// Keys should follow OpenTelemetry's attribute semantic conventions:
 /// https://github.com/open-telemetry/opentelemetry-specification/tree/main/specification/resource/semantic_conventions
 class ResourceAttributes {
+  /// Name of the deployment environment or tier.
+  static const String deploymentEnvironment = 'deployment.environment';
+
   /// Logical name of the service.
   static const String serviceName = 'service.name';
 
