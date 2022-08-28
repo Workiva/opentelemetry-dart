e7662704c5ca6ed1a0a6e64aad85e0a08e48c312
dustinlessard-wf <dustin.lessard@workiva.com>
2022-08-28 09:09:39 -0400

    O11Y-2154 : return non-functional counter

--- lib/src/sdk/metrics/meter.dart
+++ lib/src/sdk/metrics/meter.dart
@@ -3,7 +3,6 @@ import 'package:opentelemetry/api.dart' as api;
 class Meter implements api.Meter {
   @override
   api.Counter createCounter<t>(String name, api.MetricOptions options) {
-    // // TODO: implement createCounter
-    throw UnimplementedError();
+    return api.Counter();
   }
 }
