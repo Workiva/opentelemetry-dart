82ab1e5e09e30eeba427ca44a592e5bc952c027d
dustinlessard-wf <dustin.lessard@workiva.com>
2022-08-28 08:58:43 -0400

    O11Y-2154 : adjusted api MeterProvider test

--- test/unit/api/metrics/meter_provider_test.dart
+++ test/unit/api/metrics/meter_provider_test.dart
@@ -1,16 +1,43 @@
-// @TestOn('vm')
-// import 'package:opentelemetry/api.dart' as api;
-// import 'package:test/test.dart';
+@TestOn('vm')
+import 'package:opentelemetry/api.dart' as api;
+import 'package:test/test.dart';
 
-// void main() {
-//   group('MeterProvider:', () {
-//     setUp(() {});
+void main() {
+  group('MeterProvider:', () {
+    setUp(() {});
 
-//     test('noop returns inert instance of Meter', () {
-//       // final provider = api.MeterProvider();
-//       // expect(provider, isA<api.NoopMeterProvider>());
-//       // final meter = provider.get('testname');
-//       // expect(meter, isA<api.NoopMeter>());
-//     });
-//   });
-// }
+    test('MeterProvider.get with name returns inert instance of Meter', () {
+      final provider = api.MeterProvider();
+      expect(provider, isA<api.MeterProvider>());
+      final meter = provider.get('testname');
+      expect(meter, isA<api.Meter>());
+    });
+    test('MeterProvider.get with name+version returns inert instance of Meter',
+        () {
+      final provider = api.MeterProvider();
+      expect(provider, isA<api.MeterProvider>());
+      final meter = provider.get('testname', instrumentationVersion: 'version');
+      expect(meter, isA<api.Meter>());
+    });
+
+    test(
+        'MeterProvider.get with name+version+url returns inert instance of Meter',
+        () {
+      final provider = api.MeterProvider();
+      expect(provider, isA<api.MeterProvider>());
+      final meter = provider.get('testname',
+          instrumentationVersion: 'version', schemaUrl: 'url');
+      expect(meter, isA<api.Meter>());
+    });
+
+    test(
+        'MeterProvider.get with name+version+url+attributes returns inert '
+        'instance of Meter', () {
+      final provider = api.MeterProvider();
+      expect(provider, isA<api.MeterProvider>());
+      final meter = provider.get('testname',
+          instrumentationVersion: 'version', schemaUrl: 'url', attributes: {});
+      expect(meter, isA<api.Meter>());
+    });
+  });
+}
