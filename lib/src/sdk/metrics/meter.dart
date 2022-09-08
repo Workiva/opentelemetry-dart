f7fd3c018e1edabcbddcf3a58779c38c352b0738
dustinlessard-wf <dustin.lessard@workiva.com>
2022-09-08 12:19:58 -0400

    O11Y-2154 : code cleanup

--- lib/src/sdk/metrics/meter.dart
+++ lib/src/sdk/metrics/meter.dart
@@ -2,8 +2,6 @@ import 'package:opentelemetry/api.dart' as api;
 import 'package:opentelemetry/sdk.dart' as sdk;
 
 class Meter implements api.Meter {
-  static const invalidMeterNameMessage = 'Invalid Meter Name';
-
   @override
   api.Counter createCounter<T>(String name, {api.MetricOptions options}) {
     return sdk.Counter<T>();
