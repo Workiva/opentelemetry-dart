c092618a8c26987b79c2d236edabb9b7762617d3
dustinlessard-wf <dustin.lessard@workiva.com>
2022-11-05 08:11:58 -0400

    WIP

--- lib/src/sdk/metrics/meter_provider.dart
+++ lib/src/sdk/metrics/meter_provider.dart
@@ -3,6 +3,7 @@ import 'package:opentelemetry/src/experimental_api.dart' as api;
 import 'package:opentelemetry/src/experimental_sdk.dart' as sdk;
 import 'package:logging/logging.dart';
 import 'package:opentelemetry/src/api/metrics/meter_key.dart';
+import 'package:opentelemetry/src/sdk/common/instrumentation_scope.dart';
 import 'package:opentelemetry/src/sdk/metrics/state/meter_provider_shared_state.dart';
 
 const invalidMeterNameMessage = 'Invalid Meter Name';
@@ -13,24 +14,20 @@ class MeterProvider implements api.MeterProvider {
   var _shutdown = false;
   final MeterProviderSharedState _sharedState;
 
-  MeterProvider({sdk.Resource resource, List<View> views})
-      : _sharedState = MeterProviderSharedState(resource) {}
-
+  MeterProvider({sdk.Resource resource})
+      : _sharedState = MeterProviderSharedState(resource);
+    
+  
   @override
   api.Meter get(String name,
       {String version = '',
       String schemaUrl = '',
       List<api.Attribute> attributes = const []}) {
-    // if (name == null || name == '') {
-    //   name = '';
-    //   _logger.warning(invalidMeterNameMessage, '', StackTrace.current);
-    // }
-    // version ??= '';
-    // schemaUrl ??= '';
-    // attributes ??= const [];
-    // final key = MeterKey(name, version, schemaUrl, attributes);
-
-    // return _meters.putIfAbsent(key, sdk.Meter());
+    if (name == null || name == '') {
+      name = '';
+      _logger.warning(invalidMeterNameMessage, '', StackTrace.current);
+    }
+    
 
     if (_shutdown) {
       _logger.warning('A shutdown MeterProvider cannot provide a Meter', '',
@@ -38,5 +35,25 @@ class MeterProvider implements api.MeterProvider {
       return api.NoopMeter();
     }
 
+    return _sharedState
+      .getMeterSharedState(InstrumentationScope(name, version, schemaUrl))
+      .meter;
+
+  }
+
+  /// Flush all buffered data and shut down the MeterProvider and all registered
+  /// MetricReaders.
+  /// Returns a future which is resolved when all flushes are complete.
+  async shutdown(options?: ShutdownOptions): Promise<void> {
+    if (this._shutdown) {
+      api.diag.warn('shutdown may only be called once per MeterProvider');
+      return;
+    }
+
+    this._shutdown = true;
+
+    await Promise.all(this._sharedState.metricCollectors.map(collector => {
+      return collector.shutdown(options);
+    }));
   }
 }
