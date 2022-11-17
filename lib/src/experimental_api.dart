b9ab121facceaace848253af9097465ec73d7790
dustinlessard-wf <dustin.lessard@workiva.com>
2022-11-17 08:26:00 -0500

    O11Y-2256: mark experimental api and sdk as experimental

--- lib/src/experimental_api.dart
+++ lib/src/experimental_api.dart
@@ -1,3 +1,8 @@
+@experimental
+library experimental_api;
+
+import 'package:meta/meta.dart';
+
 export 'api/metrics/counter.dart' show Counter;
 export 'api/metrics/meter_provider.dart' show MeterProvider;
 export 'api/metrics/meter.dart' show Meter;
--- lib/src/experimental_sdk.dart
+++ lib/src/experimental_sdk.dart
@@ -1,3 +1,8 @@
+@experimental
+library experimental_sdk;
+
+import 'package:meta/meta.dart';
+
 export 'sdk/metrics/counter.dart' show Counter;
 export 'sdk/metrics/meter_provider.dart' show MeterProvider;
 export 'sdk/metrics/meter.dart' show Meter;
