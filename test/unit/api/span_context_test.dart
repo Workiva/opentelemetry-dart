3d42e6b1194626e9a9bd2f1ae9e1591f35b59a6b
Michael Yeager <michael.yeager@webfilings.com>
2023-05-22 14:28:45 -0500

    Consolidate unit tests for SpanContext

--- test/unit/api/span_context_test.dart
+++ test/unit/api/span_context_test.dart
@@ -6,18 +6,62 @@ import 'package:opentelemetry/api.dart' as api;
 import 'package:test/test.dart';
 
 void main() {
-  test('spanContext getters', () {
-    final spanId = api.SpanId([4, 5, 6]);
-    final traceId = api.TraceId([1, 2, 3]);
-    const traceFlags = api.TraceFlags.none;
+  test('valid context evaluates as valid', () {
+    final spanId = api.SpanId([1, 2, 3]);
+    final traceId = api.TraceId([4, 5, 6]);
+    const traceFlags = api.TraceFlags.sampled;
     final traceState = api.TraceState.empty();
 
-    final spanContext =
+    final testSpanContext =
         api.SpanContext(traceId, spanId, traceFlags, traceState);
 
-    expect(spanContext.traceId, same(traceId));
-    expect(spanContext.spanId, same(spanId));
-    expect(spanContext.traceFlags, same(traceFlags));
-    expect(spanContext.traceState, same(traceState));
+    expect(testSpanContext.isValid, isTrue);
+    expect(testSpanContext.traceId, same(traceId));
+    expect(testSpanContext.spanId, same(spanId));
+    expect(testSpanContext.traceFlags, same(traceFlags));
+    expect(testSpanContext.traceState, same(traceState));
+  });
+
+  test('invalid parsed parent span ID from header creates an invalid context',
+      () {
+    final spanId = api.SpanId.fromString('0000000000000000');
+    final traceId = api.TraceId([4, 5, 6]);
+    const traceFlags = api.TraceFlags.sampled;
+    final traceState = api.TraceState.empty();
+
+    final testSpanContext =
+        api.SpanContext(traceId, spanId, traceFlags, traceState);
+
+    expect(testSpanContext.isValid, isFalse);
+    expect(testSpanContext.traceId, same(traceId));
+    expect(testSpanContext.spanId, same(spanId));
+    expect(testSpanContext.traceFlags, same(traceFlags));
+    expect(testSpanContext.traceState, same(traceState));
+  });
+
+  test('invalid parsed trace ID from header creates an invalid context', () {
+    final spanId = api.SpanId([1, 2, 3]);
+    final traceId = api.TraceId.fromString('00000000000000000000000000000000');
+    const traceFlags = api.TraceFlags.sampled;
+    final traceState = api.TraceState.empty();
+
+    final testSpanContext =
+        api.SpanContext(traceId, spanId, traceFlags, traceState);
+
+    expect(testSpanContext.isValid, isFalse);
+    expect(testSpanContext.traceId, same(traceId));
+    expect(testSpanContext.spanId, same(spanId));
+    expect(testSpanContext.traceFlags, same(traceFlags));
+    expect(testSpanContext.traceState, same(traceState));
+  });
+
+  test('invalid context evaluates as invalid', () {
+    final testSpanContext = api.SpanContext.invalid();
+
+    expect(testSpanContext.isValid, isFalse);
+    expect(testSpanContext.traceId.isValid, isFalse);
+    expect(testSpanContext.isValid, isFalse);
+    expect(testSpanContext.traceFlags, equals(api.TraceFlags.none));
+    expect(testSpanContext.traceState.isEmpty, isTrue);
   });
 }
