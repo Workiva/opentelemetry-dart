6c4314cabf2fc604dca8b4ffd6b448585300991c
Michael Yeager <michael.yeager@workiva.com>
2023-10-23 12:07:46 -0500

    O11Y-3138: Improve Adherence to Specification  - Return opentelemetry-proto to v0.9.0.

--- lib/src/sdk/proto/opentelemetry/proto/trace/v1/trace.pbenum.dart
+++ lib/src/sdk/proto/opentelemetry/proto/trace/v1/trace.pbenum.dart
@@ -1,6 +1,3 @@
-// Copyright 2021-2022 Workiva.
-// Licensed under the Apache License, Version 2.0. Please see https://github.com/Workiva/opentelemetry-dart/blob/master/LICENSE for more information
-
 ///
 //  Generated code. Do not modify.
 //  source: opentelemetry/proto/trace/v1/trace.proto
@@ -11,6 +8,21 @@
 // ignore_for_file: UNDEFINED_SHOWN_NAME
 import 'dart:core' as $core;
 import 'package:protobuf/protobuf.dart' as $pb;
+
+class SpanFlags extends $pb.ProtobufEnum {
+  static const SpanFlags SPAN_FLAGS_DO_NOT_USE = SpanFlags._(0, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'SPAN_FLAGS_DO_NOT_USE');
+  static const SpanFlags SPAN_FLAGS_TRACE_FLAGS_MASK = SpanFlags._(255, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'SPAN_FLAGS_TRACE_FLAGS_MASK');
+
+  static const $core.List<SpanFlags> values = <SpanFlags> [
+    SPAN_FLAGS_DO_NOT_USE,
+    SPAN_FLAGS_TRACE_FLAGS_MASK,
+  ];
+
+  static final $core.Map<$core.int, SpanFlags> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static SpanFlags? valueOf($core.int value) => _byValue[value];
+
+  const SpanFlags._($core.int v, $core.String n) : super(v, n);
+}
 
 class Span_SpanKind extends $pb.ProtobufEnum {
   static const Span_SpanKind SPAN_KIND_UNSPECIFIED = Span_SpanKind._(0, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'SPAN_KIND_UNSPECIFIED');
@@ -33,51 +45,6 @@ class Span_SpanKind extends $pb.ProtobufEnum {
   static Span_SpanKind? valueOf($core.int value) => _byValue[value];
 
   const Span_SpanKind._($core.int v, $core.String n) : super(v, n);
-}
-
-class Status_DeprecatedStatusCode extends $pb.ProtobufEnum {
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_OK = Status_DeprecatedStatusCode._(0, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_OK');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_CANCELLED = Status_DeprecatedStatusCode._(1, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_CANCELLED');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_UNKNOWN_ERROR = Status_DeprecatedStatusCode._(2, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_UNKNOWN_ERROR');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_INVALID_ARGUMENT = Status_DeprecatedStatusCode._(3, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_INVALID_ARGUMENT');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_DEADLINE_EXCEEDED = Status_DeprecatedStatusCode._(4, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_DEADLINE_EXCEEDED');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_NOT_FOUND = Status_DeprecatedStatusCode._(5, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_NOT_FOUND');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_ALREADY_EXISTS = Status_DeprecatedStatusCode._(6, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_ALREADY_EXISTS');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_PERMISSION_DENIED = Status_DeprecatedStatusCode._(7, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_PERMISSION_DENIED');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_RESOURCE_EXHAUSTED = Status_DeprecatedStatusCode._(8, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_RESOURCE_EXHAUSTED');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_FAILED_PRECONDITION = Status_DeprecatedStatusCode._(9, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_FAILED_PRECONDITION');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_ABORTED = Status_DeprecatedStatusCode._(10, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_ABORTED');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_OUT_OF_RANGE = Status_DeprecatedStatusCode._(11, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_OUT_OF_RANGE');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_UNIMPLEMENTED = Status_DeprecatedStatusCode._(12, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_UNIMPLEMENTED');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_INTERNAL_ERROR = Status_DeprecatedStatusCode._(13, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_INTERNAL_ERROR');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_UNAVAILABLE = Status_DeprecatedStatusCode._(14, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_UNAVAILABLE');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_DATA_LOSS = Status_DeprecatedStatusCode._(15, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_DATA_LOSS');
-  static const Status_DeprecatedStatusCode DEPRECATED_STATUS_CODE_UNAUTHENTICATED = Status_DeprecatedStatusCode._(16, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'DEPRECATED_STATUS_CODE_UNAUTHENTICATED');
-
-  static const $core.List<Status_DeprecatedStatusCode> values = <Status_DeprecatedStatusCode> [
-    DEPRECATED_STATUS_CODE_OK,
-    DEPRECATED_STATUS_CODE_CANCELLED,
-    DEPRECATED_STATUS_CODE_UNKNOWN_ERROR,
-    DEPRECATED_STATUS_CODE_INVALID_ARGUMENT,
-    DEPRECATED_STATUS_CODE_DEADLINE_EXCEEDED,
-    DEPRECATED_STATUS_CODE_NOT_FOUND,
-    DEPRECATED_STATUS_CODE_ALREADY_EXISTS,
-    DEPRECATED_STATUS_CODE_PERMISSION_DENIED,
-    DEPRECATED_STATUS_CODE_RESOURCE_EXHAUSTED,
-    DEPRECATED_STATUS_CODE_FAILED_PRECONDITION,
-    DEPRECATED_STATUS_CODE_ABORTED,
-    DEPRECATED_STATUS_CODE_OUT_OF_RANGE,
-    DEPRECATED_STATUS_CODE_UNIMPLEMENTED,
-    DEPRECATED_STATUS_CODE_INTERNAL_ERROR,
-    DEPRECATED_STATUS_CODE_UNAVAILABLE,
-    DEPRECATED_STATUS_CODE_DATA_LOSS,
-    DEPRECATED_STATUS_CODE_UNAUTHENTICATED,
-  ];
-
-  static final $core.Map<$core.int, Status_DeprecatedStatusCode> _byValue = $pb.ProtobufEnum.initByValue(values);
-  static Status_DeprecatedStatusCode? valueOf($core.int value) => _byValue[value];
-
-  const Status_DeprecatedStatusCode._($core.int v, $core.String n) : super(v, n);
 }
 
 class Status_StatusCode extends $pb.ProtobufEnum {
