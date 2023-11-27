20b0a15cb8af2ca40019be75b8a3567c521bd669
Michael Yeager <michael.yeager@workiva.com>
2023-11-27 16:14:45 -0600

    O11Y-3736: opentelemetry-dart: Deprecate MeterSharedState

--- lib/src/sdk/metrics/meter.dart
+++ lib/src/sdk/metrics/meter.dart
@@ -1,16 +1,15 @@
 // Copyright 2021-2022 Workiva.
 // Licensed under the Apache License, Version 2.0. Please see https://github.com/Workiva/opentelemetry-dart/blob/master/LICENSE for more information
 
-import 'package:opentelemetry/src/experimental_sdk.dart' as sdk;
-import 'package:opentelemetry/src/experimental_api.dart' as api;
-
-import 'state/meter_shared_state.dart';
+import '../../../sdk.dart' as sdk;
+import '../../experimental_sdk.dart' as sdk;
+import '../../experimental_api.dart' as api;
 
 class Meter implements api.Meter {
-  // ignore: unused_field
-  final MeterSharedState _state;
+  final sdk.Resource _resource; // ignore: unused_field
+  final sdk.InstrumentationScope _instrumentationScope; // ignore: unused_field
 
-  Meter(this._state);
+  Meter(this._resource, this._instrumentationScope);
 
   @override
   api.Counter<T> createCounter<T extends num>(String name,
