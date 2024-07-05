1fee42ff62646fb57ca33962dbbe046c1215bfce
blakeroberts-wk <blake.roberts@workiva.com>
2024-07-05 13:08:16 -0600

    add unit tests for tracer.startSpan() with newRoot

---
+++ test/unit/sdk/trace/tracer_test.dart
@@ -0,0 +1,35 @@
+@TestOn('vm')
+
+import 'package:opentelemetry/api.dart' as api;
+import 'package:opentelemetry/src/experimental_api.dart';
+import 'package:opentelemetry/src/sdk/trace/tracer.dart';
+import 'package:opentelemetry/src/sdk/trace/tracer_provider.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('startSpan', () {
+    final tracer = TracerProviderBase().getTracer('test');
+
+    test('with newRoot true', () {
+      final parent = tracer.startSpan('parent');
+      final context = api.contextWithSpan(globalContextManager.active, parent);
+      final span =
+          (tracer as Tracer).startSpan('', newRoot: true, context: context);
+      expect(span.parentSpanId.isValid, isFalse);
+      expect(span.spanContext.traceId.isValid, isTrue);
+      expect(
+          span.spanContext.traceId, isNot(equals(parent.spanContext.traceId)));
+    });
+
+    test('with newRoot false', () {
+      final parent = tracer.startSpan('parent');
+      final context = api.contextWithSpan(globalContextManager.active, parent);
+      final span =
+          (tracer as Tracer).startSpan('', newRoot: false, context: context);
+      expect(span.parentSpanId.isValid, isTrue);
+      expect(span.parentSpanId, equals(parent.spanContext.spanId));
+      expect(span.spanContext.traceId.isValid, isTrue);
+      expect(span.spanContext.traceId, equals(parent.spanContext.traceId));
+    });
+  });
+}
