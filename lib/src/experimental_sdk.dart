8a53b9a687ecd82bb1ac2df4ea68b5b061479ed9
yusuf <myusufrosman@gmail.com>
2025-01-18 09:38:36 +0800

    wip: add BatchLogRecordProcessor

--- lib/src/experimental_sdk.dart
+++ lib/src/experimental_sdk.dart
@@ -6,11 +6,13 @@ library experimental_sdk;
 
 import 'package:meta/meta.dart';
 
+export 'sdk/logs/export_result.dart' show ExportResult, ExportResultCode;
+export 'sdk/logs/exporters/log_record_exporter.dart' show LogRecordExporter;
 export 'sdk/logs/log_record.dart' show ReadableLogRecord, ReadWriteLogRecord, LogRecord;
 export 'sdk/logs/log_record_limit.dart' show LogRecordLimits;
 export 'sdk/logs/logger.dart' show Logger;
 export 'sdk/logs/logger_provider.dart' show LoggerProvider;
-export 'sdk/logs/export_result.dart' show ExportResult, ExportResultCode;
+export 'sdk/logs/processors/batch_log_record_processor.dart' show BatchLogRecordProcessor;
 export 'sdk/logs/processors/log_record_processor.dart' show LogRecordProcessor;
 export 'sdk/metrics/counter.dart' show Counter;
 export 'sdk/metrics/meter.dart' show Meter;
