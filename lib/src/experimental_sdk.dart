030858bf733643da0abeff513ae2e43e278ae65a
yusuf <myusufrosman@gmail.com>
2025-01-18 10:07:55 +0800

    wip: add ConsoleLogRecordExporter

--- lib/src/experimental_sdk.dart
+++ lib/src/experimental_sdk.dart
@@ -7,6 +7,7 @@ library experimental_sdk;
 import 'package:meta/meta.dart';
 
 export 'sdk/logs/export_result.dart' show ExportResult, ExportResultCode;
+export 'sdk/logs/exporters/console_log_record_exporter.dart' show ConsoleLogRecordExporter;
 export 'sdk/logs/exporters/log_record_exporter.dart' show LogRecordExporter;
 export 'sdk/logs/log_record.dart' show ReadableLogRecord, ReadWriteLogRecord, LogRecord;
 export 'sdk/logs/log_record_limit.dart' show LogRecordLimits;
