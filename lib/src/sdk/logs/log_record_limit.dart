38b213706a4d0fbd2156f56356213db838d1273f
yusuf <myusufrosman@gmail.com>
2025-01-21 01:47:10 +0800

    wip: attach copyright

--- lib/src/sdk/logs/log_record_limit.dart
+++ lib/src/sdk/logs/log_record_limit.dart
@@ -1,3 +1,6 @@
+// Copyright 2021-2022 Workiva.
+// Licensed under the Apache License, Version 2.0. Please see https://github.com/Workiva/opentelemetry-dart/blob/master/LICENSE for more information
+
 // https://opentelemetry.io/docs/specs/otel/logs/sdk/#logrecord-limits
 abstract class LogRecordLimits {
   // https://opentelemetry.io/docs/specs/otel/common/#configurable-parameters
--- lib/src/sdk/logs/logger_config.dart
+++ lib/src/sdk/logs/logger_config.dart
@@ -1,3 +1,6 @@
+// Copyright 2021-2022 Workiva.
+// Licensed under the Apache License, Version 2.0. Please see https://github.com/Workiva/opentelemetry-dart/blob/master/LICENSE for more information
+
 // https://opentelemetry.io/docs/specs/otel/logs/sdk/#loggerconfig
 class LoggerConfig {
   /// If not explicitly set,
