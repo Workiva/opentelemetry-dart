849e0b0d615a6555425252540027c9d0a8bd4002
yusuf <myusufrosman@gmail.com>
2025-02-13 10:48:55 +0800

    wip: fix test

--- test/unit/sdk/logs/logger_provider_test.dart
+++ test/unit/sdk/logs/logger_provider_test.dart
@@ -14,8 +14,7 @@ import '../../mocks.dart';
 void main() {
   setUpAll(() {
     registerFallbackValue(sdk.LogRecord(
-      instrumentationScope: sdk.InstrumentationScope(
-          'library_name', 'library_version', 'url://schema', []),
+      instrumentationScope: sdk.InstrumentationScope('library_name', 'library_version', 'url://schema', []),
       logRecordLimits: LogRecordLimitsImpl(),
     ));
   });
@@ -23,13 +22,13 @@ void main() {
   test('traceProvider custom timeProvider', () {
     final mockTimeProvider = FakeTimeProvider(now: Int64(123));
     final mockProcessor1 = MockLogRecordProcessor();
-    final provider = sdk.LoggerProvider(
-        timeProvider: mockTimeProvider, processors: [mockProcessor1]);
+    final provider = sdk.LoggerProvider(timeProvider: mockTimeProvider, processors: [mockProcessor1]);
     provider.get('foo').emit();
     verify(() => mockProcessor1.onEmit(any(
           that: predicate((a) {
             if (a is! sdk.ReadWriteLogRecord) return false;
-            return a.timeStamp == DateTime.fromMicrosecondsSinceEpoch(123 ~/ 1000) && a.observedTimestamp == DateTime.fromMicrosecondsSinceEpoch(123 ~/ 1000);
+            return a.timeStamp == DateTime.fromMicrosecondsSinceEpoch(123 ~/ 1000) &&
+                a.observedTimestamp == DateTime.fromMicrosecondsSinceEpoch(123 ~/ 1000);
           }),
         ))).called(1);
   });
@@ -39,8 +38,7 @@ void main() {
     final mockProcessor2 = MockLogRecordProcessor();
     when(mockProcessor1.forceFlush).thenAnswer((_) async => Future.value());
     when(mockProcessor2.forceFlush).thenAnswer((_) async => Future.value());
-    await sdk.LoggerProvider(processors: [mockProcessor1, mockProcessor2])
-        .forceFlush();
+    sdk.LoggerProvider(processors: [mockProcessor1, mockProcessor2]).forceFlush();
 
     verify(mockProcessor1.forceFlush).called(1);
     verify(mockProcessor2.forceFlush).called(1);
@@ -51,8 +49,7 @@ void main() {
     final mockProcessor2 = MockLogRecordProcessor();
     when(mockProcessor1.shutdown).thenAnswer((_) async => Future.value());
     when(mockProcessor2.shutdown).thenAnswer((_) async => Future.value());
-    await sdk.LoggerProvider(processors: [mockProcessor1, mockProcessor2])
-        .shutdown();
+    sdk.LoggerProvider(processors: [mockProcessor1, mockProcessor2]).shutdown();
 
     verify(mockProcessor1.shutdown).called(1);
     verify(mockProcessor2.shutdown).called(1);
