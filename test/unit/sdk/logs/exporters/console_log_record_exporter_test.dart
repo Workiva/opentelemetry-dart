f81e28ebd3c15f163e6e501e2719744041db2f2b
yusuf <myusufrosman@gmail.com>
2025-03-03 10:40:49 +0800

    fix: test

--- test/unit/sdk/logs/exporters/console_log_record_exporter_test.dart
+++ test/unit/sdk/logs/exporters/console_log_record_exporter_test.dart
@@ -5,6 +5,7 @@
 import 'package:fixnum/fixnum.dart';
 import 'package:opentelemetry/api.dart' as api;
 import 'package:opentelemetry/sdk.dart' as sdk;
+import 'package:opentelemetry/src/experimental_api.dart' as api;
 import 'package:opentelemetry/src/experimental_sdk.dart' as sdk;
 import 'package:opentelemetry/src/sdk/logs/log_record_limit.dart';
 import 'package:test/test.dart';
@@ -22,6 +23,8 @@ void main() {
       'Test exporter',
       overridePrint(
         () async {
+          final timeProvider = FakeTimeProvider(now: Int64(123));
+          final severityDefault = api.Severity.unspecified;
           final exporter = sdk.ConsoleLogRecordExporter();
           final tracer = sdk.TracerProviderBase().getTracer('test');
           final parent = tracer.startSpan('parent');
@@ -30,7 +33,7 @@ void main() {
             instrumentationScope: sdk.InstrumentationScope('library_name', 'library_version', 'url://schema', []),
             context: context,
             logRecordLimits: LogRecordLimits(),
-            timeProvider: FakeTimeProvider(now: Int64(123)),
+            timeProvider: timeProvider,
             resource: sdk.Resource([api.Attribute.fromString('resource.name', 'test')]),
           )
             ..makeReadonly()
@@ -40,7 +43,7 @@ void main() {
           await exporter.export([logRecord]);
 
           expect(log, [
-            '{resource: {attributes: {resource.name: test}}, instrumentationScope: {name: library_name, version: library_version, schemaUrl: url://schema, attributes: {}}, timestamp: 123, severityText: null, severityNumber: null, body: null, attributes: {}, traceId: ${spanContext.traceId}, spanId: ${spanContext.spanId}, traceFlags: ${spanContext.traceFlags}}'
+            '{resource: {attributes: {resource.name: test}}, instrumentationScope: {name: library_name, version: library_version, schemaUrl: url://schema, attributes: {}}, timestamp: ${DateTime.fromMicrosecondsSinceEpoch(timeProvider.now.toInt() ~/ 1000)}, severityText: ${severityDefault.name}, severityNumber: $severityDefault, body: null, attributes: {}, traceId: ${spanContext.traceId}, spanId: ${spanContext.spanId}, traceFlags: ${spanContext.traceFlags}}'
           ]);
 
           await exporter.shutdown();
