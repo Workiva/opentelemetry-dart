b397a0ab388ff712ca10df9965301a304d14d0c4
yusuf <myusufrosman@gmail.com>
2025-05-06 14:46:12 +0800

    wip: remove getters

--- lib/src/sdk/logs/log_record_limit.dart
+++ lib/src/sdk/logs/log_record_limit.dart
@@ -2,16 +2,11 @@
 // Licensed under the Apache License, Version 2.0. Please see https://github.com/Workiva/opentelemetry-dart/blob/master/LICENSE for more information
 
 class LogRecordLimits {
-  final int _attributeCountLimit;
-  final int _attributeValueLengthLimit;
+  final int attributeCountLimit;
+  final int attributeValueLengthLimit;
 
   const LogRecordLimits({
-    int attributeCountLimit = 128,
-    int attributeValueLengthLimit = -1,
-  })  : _attributeCountLimit = attributeCountLimit,
-        _attributeValueLengthLimit = attributeValueLengthLimit;
-
-  int get attributeCountLimit => _attributeCountLimit;
-
-  int get attributeValueLengthLimit => _attributeValueLengthLimit;
+    this.attributeCountLimit = 128,
+    this.attributeValueLengthLimit = -1,
+  });
 }
